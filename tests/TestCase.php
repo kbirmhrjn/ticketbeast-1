dcb918fbbd3cef6937d8cd92fc8edcdd69aa2b39
Matthew Carson <matthew24carson@gmail.com>
2016-12-27 03:17:33 -0500

    Email validation test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Exceptions\Handler;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+
 abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
 {
     /**
@@ -22,4 +25,15 @@ abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
 
         return $app;
     }
+
+    protected function disableExceptionHandling()
+	{
+		$this->app->instance(ExceptionHandler::class, new class extends Handler {
+			public function __construct() {}
+			public function report(Exception $e) {}
+			public function render($request, Exception $e) {
+				throw $e;
+			}
+		});
+	}
 }
