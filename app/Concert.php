49a33db827a78afb47c78e709f2ab7405dada3b8
Matthew Carson <matthew24carson@gmail.com>
2016-12-26 20:27:50 -0500

    Refactor ticket ordering

--- app/Concert.php
+++ app/Concert.php
@@ -34,4 +34,19 @@ class Concert extends Model
 	{
 		return $this->hasMany(Order::class);
 	}
+
+	public function orderTickets($email, $ticketQuantity)
+	{
+		// Creating the order
+		$order = $this->orders()->create([
+			'email' => $email
+		]);
+
+		foreach (range(1, $ticketQuantity) as $i)
+		{
+			$order->tickets()->create([]);
+		}
+
+		return $order;
+	}
 }
