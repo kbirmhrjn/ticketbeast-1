7b3238d6cfc8192f8d095b71e36df5e72e7c70b5
Matthew Carson <matthew24carson@gmail.com>
2017-01-18 16:47:34 -0500

    Return order details in JSON

--- app/Order.php
+++ app/Order.php
@@ -9,6 +9,16 @@ class Order extends Model
 	protected $guarded = [];
 
 	/**
+	 * An order belongs to a single concert.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
+	public function concert()
+	{
+		return $this->belongsTo(Concert::class);
+	}
+
+	/**
 	 * An order has many tickets.
 	 *
 	 * @return \Illuminate\Database\Eloquent\Relations\HasMany
@@ -40,5 +50,14 @@ class Order extends Model
 		}
 
 		$this->delete();
+	}
+
+	public function toArray()
+	{
+		return [
+			'email' => $this->email,
+			'ticket_quantity' => $this->ticketQuantity(),
+			'amount' => $this->ticketQuantity() * $this->concert->ticket_price
+		];
 	}
 }
