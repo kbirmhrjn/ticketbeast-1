27746e936a95dbf7aaab6fdfd7450f4b3c761a9c
Matthew Carson <matthew24carson@gmail.com>
2017-01-19 15:20:19 -0500

    Remove reliance on database from ReservationTest

--- tests/unit/ReservationTest.php
+++ tests/unit/ReservationTest.php
@@ -13,11 +13,11 @@ class ReservationTest extends TestCase
     /** @test **/
     public function calculating_the_total_cost()
     {
-        $concert = factory(Concert::class)->create([
-        	'ticket_price' => 1200
-		])->addTickets(3);
-
-        $tickets = $concert->findTickets(3);
+		$tickets = collect([
+			(object) ['price' => 1200],
+			(object) ['price' => 1200],
+			(object) ['price' => 1200]
+		]);
 
         $reservation = new Reservation($tickets);
 
